50c84b705a762b3a89688112cbd95323ca3c33eb
12ed3e34dw <pvlad8056@gmail.com>
2025-09-03 03:15:19 +0300

    init

--- src/app/Adults/page.tsx
+++ src/app/Adults/page.tsx
@@ -34,30 +34,27 @@ export default function Page() {
                 <div className="txt_CHOOSE">{t("CHOOSE")}</div>
                 <div className="txt_US">{t("US?")}</div>
                 <div className="txt">{t("We are comfortable")}</div>
-
                 <div className="reasons">
                     <div className="block_teachnologies">
                         <div className="txt_14">01</div>
                       <div className="txt_15">{t("MODERN TECHNOLOGIES")}</div>
-
                         <div className="txt_16">{t("tasks")}</div>
                         <div className="txt_17">{t("lessons")}</div>
                         <div className="txt_18">{t("sweeties")}</div>
-
                     </div>
                     <div className="block_program">
                      <div className="txt_19">02</div>
-                        <div className="txt_20">{t("txt_20")}</div>
-                        <div className="txt_21">{t("txt_21")}</div>
-                        <div className="txt_22">{t("txt_22")}</div>
-                        <div className="txt_23">{t("txt_23")}</div>
+                        <div className="txt_20">{t("INDIVIDUAL PROGRAM")}</div>
+                        <div className="txt_21">{t("Learn English anywhere and anytime -you will")}</div>
+                        <div className="txt_22">{t("recieve a comfortable teacher and usefull")}</div>
+                        <div className="txt_23">{t("materials for every lesson.")}</div>
                     </div>
                     <div className="block_perfect">
                        <div className="txt_24">03</div>
-                        <div className="txt_25">{t("txt_25")}</div>
-                        <div className="txt_26">{t("txt_26")}</div>
-                        <div className="txt_27">{t("txt_27")}</div>
-                        <div className="txt_28">{t("txt_28")}</div>
+                        <div className="txt_25">{t("PERFECT ATMOSPHERE")}</div>
+                        <div className="txt_26">{t("Interactive lessons won`t let you get bored,and")}</div>
+                        <div className="txt_27">{t("teachers conduct lessons both in a business or in a")}</div>
+                        <div className="txt_28">{t("playful way.")}</div>
                     </div>
                 </div>
             </div>
@@ -65,67 +62,116 @@ export default function Page() {
             {/* Третий экран - Statistics */}
             <div className="content-section">
                 <div className="Statistics">
-
                     <div className="txt_1">2023</div>
                     <div className="txt_2">{t("the best school of the")}</div>
                     <div className="txt_3">{t("year")}</div>
-
-
-
-
                     <div className="txt_4">50K+</div>
                     <div className="txt_5">{t("happy pupils")}</div>
-
                     <div className="txt_6">300+</div>
                     <div className="txt_7">{t("teachers in team")}</div>
-
                     <div className="txt_8">AI</div>
                     <div className="txt_9">{t("unique education")}</div>
                     <div className="txt_10">{t("programm")}</div>
-
                     <div className="txt_11">10</div>
                     <div className="txt_12">{t("years we shine in the")}</div>
                     <div className="txt_13">{t("world!")}</div>
-
                     <div className="hr_1"></div>
                     <div className="hr_2"></div>
                     <div className="hr_3"></div>
                     <div className="hr_4"></div>
-
-
-
-
-
                 </div>
             </div>
-
-
             <div className="content_2">
-                <div className="txt_29">{t("txt_29")}</div>
-                <div className="txt_30">{t("txt_30")}</div>
-
-                <div className="txt_31">{t("txt_31")}</div>
-
-                <div className="txt_32">{t("txt_32")}</div>
-                <div className="txt_33">{t("txt_33")}</div>
-
+                <div className="txt_29">{t("SIGN UP")}</div>
+                <div className="txt_30">{t("FOR A")}</div>
+                <div className="txt_31">{t("FREE SESSION")}</div>
+                <div className="txt_32">{t("Log in our platform,take a short test of your preferences and get the lesson")}</div>
+                <div className="txt_33">{t("for free")}</div>
                 <button className="button_2">
                     <div className="txt_button_2">
-                        {t("txt_bu")}
+                        {t("Let`s go!")}
                     </div>
                 </button>
             </div>
-
-
             <div className="content_3">
-                <div className="txt_34">{t("txt_34")}</div>
-                <div className="txt_35">{t("txt_35")}</div>
-
-                <div className="txt_36">{t("txt_36")}</div>
+                <div className="txt_34">{t("OUR")}</div>
+                <div className="txt_35">{t("COURSES")}</div>
+                <div className="txt_36">{t("The most popular")}</div>
+            </div>
+    <div className="course_sections">
+        <div className="container_4">
+<div className="txt_37">
+    {t("TOURIST ASSISTANT")}
+</div>
+   <div className="txt_38">
+       {t("Extra course for new, which give basic phrases")}
+   </div>
+            <div className="txt_39">
+                {t("-give preparation for the culture specifieces")}
+            </div>
+     <div className="txt_40">
+         {t("-make more confident")}
+     </div>
+            <div className="txt_41">
+                {t("-for B1-B2")}
+            </div>
+            <div className="txt_42">
+                {t("15 SEPTEMBER")}
+            </div>
+        </div>
+        <div className="content_5">
+<div className="txt_43">
+    {t("TOURIST ASSISTANT")}
+</div>
+           <div className="txt_44">
+               {t(" Extra course for new tourists, which give basic ")}
+           </div>
+            <div className="txt_45">
+                {t(" phrases for unexpected situations")}
             </div>
 
+            <div className="txt_46">
+                {t("-give preparation for the culture specifieces")}
+            </div>
+            <div className="txt_47">
+                {t("-make more confident")}
+            </div>
+            <div className="txt_48">
+                {t("-for B1-B2")}
+            </div>
+            <div className="txt_49">
+                {t("15 SEPTEMBER")}
+            </div>
+        </div>
+        <div className="container_6">
+<div className="txt_50">
+    {t("TOURIST ASSISTANT")}
+</div>
+     <div className="txt_51">
+         {t("Extra course for new tourists, which give basic")}
+     </div>
+          <div className="txt_52">
+              {t("phrases for unexpected situations")}
+          </div>
+            <div className="txt_53">
+                {t("-give preparation for the culture specifieces")}
+            </div>
+            <div className="txt_54">
+                {t("-for B1-B2")}
+            </div>
+            <div className="txt_55">
+                {t("15 SEPTEMBER")}
+            </div>
+        </div>
 
 
+
+
+
+        <div className="content_7">
+
+        </div>
+    </div>
 
 
 
